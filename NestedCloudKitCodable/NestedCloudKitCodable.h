8c63146e701b02af01a851978c44f0cd8951b506
Guilherme Girotto <guiga741@gmail.com>
2018-11-22 11:18:02 -0200

    adds Framework target

---
+++ NestedCloudKitCodable/NestedCloudKitCodable.h
@@ -0,0 +1,17 @@
+//
+//  NestedCloudKitCodable.h
+//  NestedCloudKitCodable
+//
+//  Created by Guilherme Girotto on 22/11/18.
+//  Copyright © 2018 Guilherme Girotto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NestedCloudKitCodable.
+FOUNDATION_EXPORT double NestedCloudKitCodableVersionNumber;
+
+//! Project version string for NestedCloudKitCodable.
+FOUNDATION_EXPORT const unsigned char NestedCloudKitCodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NestedCloudKitCodable/PublicHeader.h>
